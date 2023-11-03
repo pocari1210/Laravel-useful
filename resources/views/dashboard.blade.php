6ad588e132aa1f37e406fa2a18a13101f260acfa
pocari1210 <pocari.design@gmail.com>
2023-11-03 12:32:32 +0900

    YajraDatabase導入

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,10 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900">
-          <table class="table">
+
+          {{-- コメントアウト:ここから
+
+            <table class="table">
             <thead>
               <tr>
                 <th scope="col">#</th>
@@ -23,21 +26,27 @@
               @foreach($users as $user)
               <tr>
                 <th scope="row">{{ ++$loop->index }}</th>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->created_at }}</td>
-                <td>{{ $user->update_at }}</td>
-              </tr>
-              @endforeach
-            </tbody>
+          <td>{{ $user->name }}</td>
+          <td>{{ $user->email }}</td>
+          <td>{{ $user->created_at }}</td>
+          <td>{{ $user->update_at }}</td>
+          </tr>
+          @endforeach
+          </tbody>
           </table>
 
           {{$users->links()}}
+
+          コメントアウト:ここまで--}}
+
+          <!-- ★YajraDBを用いる場合のコード★-->
+          {{ $dataTable->table() }}
+
         </div>
       </div>
     </div>
   </div>
 
-
-
+  <!-- $dataTableをロードしている -->
+  {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\DataTables\UsersDataTable;
 use App\Models\User;
 
 /*
@@ -19,10 +20,14 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard', function (UsersDataTable $dataTable) {
   // $users = User::all();
-  $users = User::paginate(10);
-  return view('dashboard', compact('users'));
+  // $users = User::paginate(10);
+  // return view('dashboard', compact('users'));
+
+  // ★YajraDBを使用する場合のreturnの処理★
+  // renderの中にview名を記述する
+  return $dataTable->render('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
